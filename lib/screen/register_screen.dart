93b53f9e9ed10ccfd895e1cf83616549da27d49c
yysrll <yusril.uzumaki@gmail.com>
2023-03-06 20:02:06 +0800

    renderflex overflowed flutter when keyboard appears

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -31,6 +31,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
